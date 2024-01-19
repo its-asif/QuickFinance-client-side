4250160adc513415a02150393b8a1943bd842104
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-19 18:20:29 +0600

    [fix](hoverIssue):login and Register page text hover and login register button hover background color change

--- src/pages/login-register/register/Register.jsx
+++ src/pages/login-register/register/Register.jsx
@@ -121,7 +121,7 @@ const Register = () => {
                                     <div>
                                     <button
                                         type="button"
-                                        className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                                        className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-[#399b53]"
                                         onClick={() => { window.location.href = '/'; }
                                         }
                                     >
@@ -138,7 +138,7 @@ const Register = () => {
                                 {/* Google */}
                             <button
                                 type="button"
-                                className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-white focus:bg-gray-100 focus:text-white focus:outline-none"
+                                className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-200 hover:text-black focus:bg-gray-100 focus:text-white focus:outline-none"
                             >
                                 <span className="mr-2 inline-block">
                                 <svg
@@ -157,7 +157,7 @@ const Register = () => {
                             {/* Facebook */}
                             <button
                                 type="button"
-                                className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-white focus:bg-gray-100 focus:text-white focus:outline-none"
+                                className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-200 hover:text-black focus:bg-gray-100 focus:text-white focus:outline-none"
                             >
                                 <span className="mr-2 inline-block">
                                 <svg
