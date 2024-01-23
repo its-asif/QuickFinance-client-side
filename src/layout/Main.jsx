bc218773eb3052ed52e2370686ea7e68012235fd
Tahsin0909 <tajisan789@gmail.com>
2024-01-24 01:15:35 +0600

    Added NAvbar and Footer to all page

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/shared/navbar/Navbar";
+import Footer from "../pages/shared/footer/Footer";
 
 const Main = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
