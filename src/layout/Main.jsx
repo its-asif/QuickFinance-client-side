f8cb78a632358c012e400ccdd35cde0f3fedde2e
Tahsin0909 <tajisan789@gmail.com>
2024-01-24 01:12:18 +0600

    All done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,14 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../pages/shared/navbar/Navbar";
-import Footer from "../pages/shared/footer/Footer";
 
 const Main = () => {
     return (
         <div>
-            <Navbar />
             <Outlet />
-            <Footer />
-
         </div>
     );
 };
