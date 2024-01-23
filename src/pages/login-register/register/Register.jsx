4c6660cfafd7b4289925a40bb86a5bede84c583d
Tahsin0909 <tajisan789@gmail.com>
2024-01-24 00:10:29 +0600

    Updated register design

--- src/pages/login-register/register/Register.jsx
+++ src/pages/login-register/register/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
                     {/* Right side */}
                     <div className='bg-[#07123a]  rounded-2xl'>
                         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-10 lg:px-8 lg:py-10 ">
-                            <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+                            <div className="">
                                 <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl">Sign up</h2>
 
                                 {/* link to Login page */}
@@ -60,7 +60,7 @@ const Register = () => {
                                             </div>
 
                                             {/* Upload Image */}
-                                            <div className="grid w-full items-center gap-1.5">
+                                            <div className="grid  gap-1.5">
                                                 <label
                                                     className="text-base font-medium text-gray-100"
                                                 >
@@ -68,7 +68,7 @@ const Register = () => {
                                                 </label
                                                 >
                                                 <input
-                                                    className="flex h-10  lg:w-[200px] rounded-md border border-blue-300 border-input bg-white text-sm text-gray-400 file:h-10 file:border-0 file:bg-blue-600 file:text-white file:text-sm file:font-medium"
+                                                    className="flex h-10 lg:w-[233px] rounded-md border border-blue-300 border-input bg-white text-sm text-gray-400 file:h-10 file:border-0 file:bg-blue-600 file:text-white file:text-sm file:font-medium"
                                                     type="file"
                                                     id="picture"
                                                 />
