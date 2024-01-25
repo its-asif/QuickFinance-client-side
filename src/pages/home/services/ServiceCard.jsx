7331ee1f1c99b1f9e035860b2e68354fd22e31c0
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-25 17:06:26 +0600

    [fix](service):card title color and background change

--- src/pages/home/services/ServiceCard.jsx
+++ src/pages/home/services/ServiceCard.jsx
@@ -6,7 +6,7 @@ const ServiceCard = ({item}) => {
     
     return (
         
-        <div data-aos="fade-up" data-aos-duration="1000" className="relative card  transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0 rounded-xl bg-gradient-to-r from-[#dbdddc] to-[#b8b8b8] hover:from-[#2ef80f] hover:to-[#28f7e6] bg-clip-border text-gray-700 shadow-xl">
+        <div data-aos="fade-up" data-aos-duration="1000" className="relative card  transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0 rounded-xl bg-gradient-to-r from-[#dbdddc] to-[#b8b8b8] hover:from-[#99ffa5] hover:to-[#70f3e8] bg-clip-border text-gray-700 shadow-xl">
            <figure className="">
            <img
            src={item?.image} className=" object-cover  lg:h-[265px] w-full "
@@ -17,7 +17,7 @@ const ServiceCard = ({item}) => {
         </div> */}
         <div className="p-6">
          <div className="mb-2 ">
-           <p className="block font-sans font-semibold overflow-y-hidden uppercase leading-relaxed text-blue-gray-900 antialiased transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none text-white ">
+           <p className="block font-sans font-semibold overflow-y-hidden uppercase leading-relaxed text-blue-gray-900 antialiased transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none text-gray-700">
              {item?.category}
            </p>
            
