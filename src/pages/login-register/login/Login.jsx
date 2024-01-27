25def21ce4a36fa58530c9a1c2360e5b5598ce92
Tahsin0909 <tajisan789@gmail.com>
2024-01-28 01:19:08 +0600

    Added demo design

--- src/pages/login-register/login/Login.jsx
+++ src/pages/login-register/login/Login.jsx
@@ -102,8 +102,8 @@ const Login = () => {
 
 
 
-                    {/* Right side */}
-                    <div className='bg-[#07123a]  rounded-2xl'>
+                    {/* Right side  bg-[#07123a]*/}
+                    <div className=' trial rounded-2xl'>
                         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-10 lg:px-8 lg:py-10 ">
                             <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
                                 <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl">Login</h2>
