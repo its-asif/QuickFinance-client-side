a65d749ee40a50140e77a3f5e10a27717d27fd8b
Tahsin0909 <tajisan789@gmail.com>
2024-02-01 20:21:11 +0600

    Improbed Tax and Zakat

--- src/pages/zakatAndTax/Zakat/Zakat.jsx
+++ src/pages/zakatAndTax/Zakat/Zakat.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FaBangladeshiTakaSign } from "react-icons/fa6";
+import { BsCalculatorFill } from "react-icons/bs";
 const Zakat = () => {
 
     const [earn, setEarn] = useState(null)
@@ -59,11 +60,11 @@ const Zakat = () => {
 
     return (
         <div>
-            <button className="px-6 py-2 mt-8 rounded text-white text-sm tracking-wider font-medium outline-none border-2 border-[#399b53] bg-[#399b53] btn hover:bg-transparent hover:text-black transition-all duration-300" onClick={handleClose}>Calculate Your Zakat</button>
+            <button className="sharedBtn flex items-center gap-1" onClick={handleClose}><span>Calculate Zakat</span> <BsCalculatorFill /></button>
 
             <dialog id="my_modal_3" className="modal">
 
-                <div className="modal-box w-11/12 max-w-5xl p-10 rounded-none">
+                <div className="modal-box w-11/12 max-w-5xl p-10 rounded-none modalBg">
 
                     <form method="dialog">
                         {/* if there is a button in form, it will close the modal */}
@@ -157,43 +158,41 @@ const Zakat = () => {
                                 </div>
 
                             </div>
-
-                            <input onClick={() => document.getElementById('my_modal_2').showModal()} type="submit" value="Calculate Zakat" className="mt-4 btn w-full bg-[#399b53] text-white rounded-none" />
+                            <button
+                                onClick={() => { document.getElementById('my_modal_2').showModal(), document.getElementById('my_modal_3').close() }} type="submit"className="fullWidthSharedBtn flex items-center gap-1">
+                                <span>Calculate Your Zakat</span> <BsCalculatorFill />
+                            </button>
                         </form>
                     </div>
-
-                    {/* modal body  */}
-                    <dialog id="my_modal_2" className="modal">
-                        <div className="modal-box">
-                            <div>
-
-                                <div className="flex flex-col w-2/3 py-8 mx-auto text-lg">
-                                    <div className="">
-                                        <p className="mb-2 flex justify-between">Total Earn : <span className="flex justify-center items-center gap-1">{earn} <FaBangladeshiTakaSign /></span></p>
-
-                                        <p className="mb-2 flex justify-between">Total Expense : <span className="flex justify-center items-center gap-1">{expense} <FaBangladeshiTakaSign /></span></p>
-
-                                        <p className="mb-2 flex justify-between">Current Amount : <span className="flex justify-center items-center gap-1">{total}<FaBangladeshiTakaSign /></span></p>
-
-                                        <p className="mb-2 flex justify-between text-xl mt-4 font-bold">Zakat Amount: <span className="flex justify-center items-center gap-1">{zakat} <FaBangladeshiTakaSign /></span></p>
-                                    </div>
-                                    {/* <button className="px-6 py-2 mt-8 rounded text-white text-sm tracking-wider font-medium outline-none border-2 border-[#399b53] bg-[#399b53] btn hover:bg-transparent hover:text-black transition-all duration-300">
-                                        Donate Your Zakat
-                                    </button> */}
-                                </div>
-
-
-                            </div>
-                        </div>
-                        <form method="dialog" className="modal-backdrop">
-                            <button>close</button>
-                        </form>
-                    </dialog>
-
-
-
                 </div>
 
+            </dialog>
+            {/* modal body  */}
+            <dialog id="my_modal_2" className="modal">
+                <div className="modal-box modalBg">
+                    <div>
+
+                        <div className="flex flex-col w-2/3 py-8 mx-auto text-lg">
+                            <div className="">
+                                <p className="mb-2 flex justify-between">Total Earn : <span className="flex justify-center items-center gap-1">{earn} <FaBangladeshiTakaSign /></span></p>
+
+                                <p className="mb-2 flex justify-between">Total Expense : <span className="flex justify-center items-center gap-1">{expense} <FaBangladeshiTakaSign /></span></p>
+
+                                <p className="mb-2 flex justify-between">Current Amount : <span className="flex justify-center items-center gap-1">{total}<FaBangladeshiTakaSign /></span></p>
+
+                                <p className="mb-2 flex justify-between text-xl mt-4 font-bold">Zakat Amount: <span className="flex justify-center items-center gap-1">{zakat} <FaBangladeshiTakaSign /></span></p>
+                            </div>
+                            {/* <button className="px-6 py-2 mt-8 rounded text-white text-sm tracking-wider font-medium outline-none border-2 border-[#399b53] bg-[#399b53] btn hover:bg-transparent hover:text-black transition-all duration-300">
+                                        Donate Your Zakat
+                                    </button> */}
+                        </div>
+
+
+                    </div>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
             </dialog>
         </div>
     );
--- src/pages/zakatAndTax/zakatAndTax/ZakatAndTax.jsx
+++ src/pages/zakatAndTax/zakatAndTax/ZakatAndTax.jsx
@@ -5,39 +5,39 @@ import taxImg from './../../../assets/zakat_tax/tax.jpg'
 
 const ZakatAndTax = () => {
     return (
-        <div className="max-w-6xl mx-auto pt-24">
+        <div className="max-w-6xl mx-auto pt-24 md:px-10 px-2 lg:px-0">
             {/* Basic Description Part  */}
 
             <div className=" text-[#333] my-4">
 
                 <div className="text-center">
-                    <h2 className="text-5xl font-extrabold text-center mb-4">Zakat and Tax Calculator</h2>
+                    <h2 className="md:text-5xl text-4xl font-extrabold text-center mb-4 primaryColor">Zakat and Tax <span className="text-black">Calculator</span>  </h2>
                     <p className="text-base mx-2 md:mx-10">Effortlessly calculate Zakat and taxes with our user-friendly tool, guiding you through financial responsibilities. Gain clarity, make informed decisions, and navigate your financial journey confidently.</p>
                 </div>
 
                 <div className="mt-16">
                     <div className="grid md:grid-cols-2 items-center gap-16">
                         <div>
-                            <img src={zakatImg} className="w-full object-contain rounded-md shadow-[0_14px_40px_-11px_rgba(93,96,127,0.2)]" />
+                            <img src="https://i.ibb.co/M9trqfM/zakat-removebg-preview.png" className="w-full object-contain rounded-md shadow-[0_14px_40px_-11px_rgba(93,96,127,0.2)]" />
                         </div>
                         <div className="text-justify mx-10 md:mx-0">
-                            <h3 className="text-2xl font-bold mb-4">Zakat : A Sacred Duty</h3>
-                            <p className="text-sm">Embrace the sacred obligation of Zakat, a fundamental pillar of Islamic giving. Our platform simplifies the Zakat calculation process, allowing you to fulfill this noble duty with ease. Explore the spiritual significance of your charitable contributions and make a positive impact on those in need</p>
+                            <h3 className="lg:text-4xl md:text-3xl text-2xl font-bold mb-4"><span className="primaryColor">Zakat</span> : A Sacred Duty</h3>
+                            <p className="text-sm mb-4 lg:mb-6  lg:text-base ">Embrace the sacred obligation of Zakat, a fundamental pillar of Islamic giving. Our platform simplifies the Zakat calculation process, allowing you to fulfill this noble duty with ease. Explore the spiritual significance of your charitable contributions and make a positive impact on those in need</p>
 
                             {/* Modal button  */}
                             <Zakat></Zakat>
                         </div>
 
                         <div className="max-md:order-1 text-justify mx-10 md:mx-0">
-                            <h3 className="text-2xl font-bold mb-4">TaxSense : Your Financial Guide</h3>
-                            <p className="text-sm">Navigate the realm of taxes with confidence using our comprehensive tax calculator. Gain valuable insights into your financial obligations, make informed decisions, and optimize your financial planning. Our user-friendly platform demystifies the complexities of taxation, ensuring a seamless and empowered approach to managing your finances.</p>
+                            <h3 className="lg:text-4xl md:text-3xl text-2xl font-bold mb-4"><span className="primaryColor">TaxSense</span> : Your Financial Guide</h3>
+                            <p className="text-sm mb-4 lg:mb-6  lg:text-base ">Navigate the realm of taxes with confidence using our comprehensive tax calculator. Gain valuable insights into your financial obligations, make informed decisions, and optimize your financial planning. Our user-friendly platform demystifies the complexities of taxation, ensuring a seamless and empowered approach to managing your finances.</p>
 
                             {/* Modal Button  */}
                             <Tax></Tax>
 
                         </div>
                         <div>
-                            <img src={taxImg} className="w-full object-contain rounded-md shadow-[0_14px_40px_-11px_rgba(93,96,127,0.2)]" />
+                            <img src="https://i.ibb.co/y6F1BKh/tax-removebg-preview-1.png" className="w-full object-contain rounded-md shadow-[0_14px_40px_-11px_rgba(93,96,127,0.2)]" />
                         </div>
                     </div>
                 </div>
