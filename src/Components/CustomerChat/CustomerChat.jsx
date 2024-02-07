5cbb9f4852b61e453b0a373d55e4d54c36d9e04d
Tahsin0909 <tajisan789@gmail.com>
2024-02-07 17:59:13 +0600

    Working on Theme on Chatbot

--- src/Components/CustomerChat/CustomerChat.jsx
+++ src/Components/CustomerChat/CustomerChat.jsx
@@ -3,7 +3,7 @@ import { CustomChat, FacebookProvider } from "react-facebook";
 const CustomerChat = () => {
     return (
         <FacebookProvider appId="718692396907673" chatSupport>
-          <CustomChat pageId="203862282818372" minimized={true}/>
+          <CustomChat pageId="203862282818372" minimized={true} themeColor="#0ba360"/>
         </FacebookProvider>    
       );
 };
