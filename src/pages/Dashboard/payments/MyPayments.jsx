e8312d4d4d8bafb684c6eb94f4c5aff9a7b528c0
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-02-21 19:07:34 +0600

    add

--- src/pages/Dashboard/payments/MyPayments.jsx
+++ src/pages/Dashboard/payments/MyPayments.jsx
@@ -74,7 +74,7 @@ const MyPayments = () => {
                                         <td>{payment.tran_id}</td>
                                         <td>
                                             {
-                                                payment.payment_status === "true" ? 
+                                                payment.payment_status === 'true'? 
                                                 <span className="btn btn-sm bg-green-200 text-green-700">Success</span> :
                                                 <span className="btn btn-sm bg-red-200 text-red-700">Failed</span>
                                             
