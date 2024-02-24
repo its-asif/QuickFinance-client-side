b86f4da25cee936c899651160cb974fcfbcad8d8
Tahsin0909 <tajisan789@gmail.com>
2024-02-24 19:49:13 +0600

    Jewelry Data sent to Backend

--- src/pages/Dashboard/MyAsset/Input/Jewelry.jsx
+++ src/pages/Dashboard/MyAsset/Input/Jewelry.jsx
@@ -2,8 +2,11 @@ import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { AiFillGolden } from "react-icons/ai";
 import { AuthContext } from "../../../../AuthProvider/Contextapi";
+import useAxiosPublic from "../../../../Hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 const Jewelry = () => {
+    const axiosPublic = useAxiosPublic()
     const { AuthUser } = useContext(AuthContext)
     const { register, handleSubmit, reset } = useForm();
 
@@ -52,8 +55,31 @@ const Jewelry = () => {
                     status: `${purchaseAmount > CurrentValue? 'down': CurrentValue > purchaseAmount ? 'ups' :'equal'}`,
                     value: CurrentValue
                 };
-                console.log(jewelryData);
-                reset()
+                axiosPublic.post('/api/assets', jewelryData)
+                .then(res => {
+                    // console.log(res.status);
+                    if (res.status === 200) {
+                        document.getElementById('my_modal_5').close();
+                        Swal.fire({
+                            title: "Successful",
+                            text: "Your Asset Added to Portfolio",
+                            icon: "success",
+                            confirmButtonColor: "#0ba360",
+                            confirmButtonText: 'DONE'
+                        });
+                        reset()
+    
+                    }
+                    else {
+                        Swal.fire({
+                            title: "oh!",
+                            text: "Some Error Occurred",
+                            icon: "error",
+                            confirmButtonColor: "#0ba360",
+                            confirmButtonText: 'DONE'
+                        });
+                    }
+                })
 
             }
             catch {
